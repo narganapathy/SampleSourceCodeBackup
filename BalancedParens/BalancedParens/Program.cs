57af170e5d0e4dc5f4c711215289ab6192825426
narg <narg@NARG-SAW-VM>
2018-11-22 12:50:17 -0800

    More test programs

---
+++ BalancedParens/BalancedParens/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedParens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Final string is {BalancedParens("(())((")}");
+            Console.ReadLine();
+        }
+
+        static string BalancedParens(string s)
+        {
+            int len = s.Length;
+            char[] stringArray = s.ToCharArray();
+            string finalString = string.Empty;
+            for (int i = 1; i< len; i++)
+            {
+                string balancedParens = GetMatchingString(stringArray, i - 1, i);
+                if (balancedParens != null)
+                {
+                    if (balancedParens.Length > finalString.Length)
+                    {
+                        finalString = balancedParens;
+                    }
+                }
+            }
+            return finalString;
+        }
+
+        static string GetMatchingString(char[] stringArray, int left, int right)
+        {
+            Console.WriteLine($"left {left} right {right}");
+            while (left >= 0 && right < stringArray.Length)
+            {
+                if ((stringArray[left] == ')' && stringArray[right] == '(') ||
+                    (stringArray[left] == '(' && stringArray[right] == ')'))
+                {
+                    Console.WriteLine($"Found matching parens {left} {right}");
+                    left--;
+                    right++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            int startIndex = left + 1;
+            int endIndex = right - 1;
+            Console.WriteLine($"startIndx {startIndex} endIndex {endIndex}");
+            if ((endIndex - startIndex) < 1) return null;
+            // parens has to be closed
+            if ((stringArray[startIndex] == ')') || (stringArray[endIndex] == '('))
+            {
+                startIndex++; endIndex--;    
+            }
+            string matchingString = new string(stringArray, startIndex, (endIndex - startIndex) + 1);
+            Console.WriteLine($"matching {matchingString}");
+            return matchingString;
+        }
+    }
+}
---
+++ NextNodeInTree/NextNodeInTree/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextNodeInTree
+{
+    class Program
+    {
+        class Node
+        {
+            public Node(string val )
+            {
+                _value = val;
+                _left = _right = _next = null;
+            }
+            public string _value;
+            public Node _left;
+            public Node _right;
+            public Node _next;
+        }
+
+        static Node BuildTree()
+        {
+            //Node root = new NextNodeInTree.Program.Node("A");
+            //root._left = new NextNodeInTree.Program.Node("B");
+            //root._right = new NextNodeInTree.Program.Node("C");
+            //root._left._left = new Node("D");
+            //root._left._right = new Node("E");
+            //root._right._left = new Node("F");
+            //root._right._right = new Node("G");
+            Node root = new NextNodeInTree.Program.Node("A");
+            root._left = new NextNodeInTree.Program.Node("B");
+            root._right = new NextNodeInTree.Program.Node("C");
+            root._left._left = new Node("D");
+            root._right._left = new Node("F");
+            return root;
+        }
+
+        static void GetMaxheightOfTree(Node root, ref int maxHeight, int currentHeight)
+        {
+            if (root == null) return;
+            if (root != null) currentHeight++;
+            maxHeight = Math.Max(maxHeight, currentHeight);
+            GetMaxheightOfTree(root._left, ref maxHeight, currentHeight); 
+            GetMaxheightOfTree(root._left, ref maxHeight, currentHeight);
+        }
+
+        static void SetCousins(Node root, Node[] siblings, int height )
+        {
+            if (root == null) return;
+            if (siblings[height] != null)
+            {
+                root._next = siblings[height];
+                siblings[height]._next = root;
+                siblings[height] = null;
+            }
+            else
+            {
+                siblings[height] = root;
+            }
+            SetCousins(root._left, siblings, height + 1);
+            SetCousins(root._right, siblings, height + 1);
+        }
+
+        static void PrintTree(Node root)
+        {
+            if (root == null) return;
+            string val, nextval;
+            val = root._value;
+            nextval = root._next != null ? root._next._value : "null";
+            Console.WriteLine($"{val} {nextval}");
+            PrintTree(root._left);
+            PrintTree(root._right);
+        }
+
+        static void Main(string[] args)
+        {
+            Node root = BuildTree();
+            PrintTree(root);
+            int maxheight = 0;
+            GetMaxheightOfTree(root, ref maxheight, 0);
+            Node[] siblings = new Node[maxheight];
+            SetCousins(root, siblings, 0);
+            PrintTree(root);
+            Console.ReadLine();
+        }
+    }
+}
